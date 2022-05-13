a7f642c533721ec73b17e646193068f9132dc2e9
yukicoding <hyq1046410512@gmial.com>
2022-05-13 23:51:13 +0800

    feat:个人中心

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,11 +1,77 @@
 import { Component } from "react";
-import { View, Text } from "@tarojs/components";
+import { View, Text, Image } from "@tarojs/components";
 import "./index.css";
-import { AtButton } from "taro-ui";
+import { AtButton, AtIcon } from "taro-ui";
+import * as images from "@/public/assets/images/pics";
+const POWER_LIST_CONFIG = {
+  coupon: {
+    image: images.coupon,
+    value: "优惠券",
+    url: "/pages/ucenter/couponList/couponList",
+  },
+  collect: {
+    image: images.collect,
+    value: "收藏",
+    url: "/pages/ucenter/collect/collect",
+  },
+  footprint: {
+    image: images.footprint,
+    value: "足迹",
+    url: "/pages/ucenter/footprint/footprint",
+  },
+  group: {
+    image: images.group,
+    value: "拼团",
+    url: "/pages/groupon/myGroupon/myGroupon",
+  },
+  address: {
+    image: images.address,
+    value: "地址",
+    url: "/pages/ucenter/address/address",
+  },
+  bindPhone: {
+    image: images.mobile,
+    value: "绑定手机",
+    url: "",
+  },
+  customer: {
+    image: images.customer,
+    value: "联系客服",
+    url: "",
+  },
+  about: {
+    image: images.about,
+    value: "关于我们",
+    url: "/pages/about/about",
+  },
+};
 const Index: any = () => {
+  const config = {
+    bgGradient: "white",
+    headPosition: "",
+  };
+  const userInfo = {
+    avatarUrl: "",
+    nickName: "qq",
+  };
   return (
-    <View className="index">
-      <Text>我的</Text>
+    <View className="layout-my">
+      <View
+        style={{
+          backgroundImage:
+            config.bgGradient === "white"
+              ? `linear-gradient(rgba(255, 255, 255, 0), rgb(255, 255, 255)), url(${images.headerBg})`
+              : `url(${images.headerBg})`,
+        }}
+        className={`header ${config.headPosition}`}
+      >
+        <Image className="avatar" src={userInfo.avatarUrl}></Image>
+        <View className="info">{userInfo.nickName}</View>
+        <View className="vip" onClick={() => console.log(1)}>
+          <View>微商城</View>
+          <AtIcon value="chevron-right" size="15" color="#fff"></AtIcon>
+        </View>
+      </View>
     </View>
   );
 };
